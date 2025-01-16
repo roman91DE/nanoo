8432e88a0fc60fc3d0abcad054ec5993166a1850
rohoehn <rohoehn123@gmail.com>
2025-01-16 16:34:37 +0100

    working on hecto tutorial

--- src/editor.rs
+++ src/editor.rs
@@ -1,30 +1,68 @@
-use crossterm::event::{read, Event::Key, KeyCode::Char};
-use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
+#![warn(clippy::all, clippy::pedantic)]
+
+use crossterm::event::{read, Event, Event::Key, KeyCode::Char, KeyEvent, KeyModifiers};
+use crossterm::execute;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType};
+use std::io::stdout;
 
 pub struct Editor {
-
+    should_quit: bool,
 }
+
 impl Editor {
-    
-    pub fn run(&self){
-        enable_raw_mode().unwrap();
+    pub fn default() -> Self {
+        Editor { should_quit: false }
+    }
+    pub fn run(&mut self) {
+        Self::initialize().unwrap();
+        let result = self.repl();
+        Self::terminate().unwrap();
+        result.unwrap();
+    }
+
+    fn initialize() -> Result<(), std::io::Error> {
+        enable_raw_mode()?;
+        Self::clear_screen()
+    }
+    fn terminate() -> Result<(), std::io::Error> {
+        disable_raw_mode()
+    }
+    fn clear_screen() -> Result<(), std::io::Error> {
+        let mut stdout = stdout();
+        execute!(stdout, Clear(ClearType::All))
+    }
+
+    fn repl(&mut self) -> Result<(), std::io::Error> {
         loop {
-            match read() {
-                Ok(Key(event)) => {
-                    println!("{:?} \r", event);
-                    match (event.code) {
-                        Char(c) => {
-                            if c == 'q' {
-                                break;
-                            }
-                        },
-                        _ => (),   
-                    }   
-                },
-                Err(err) => println!("Error: {}", err),
-                _ => ()
+            let event = read()?;
+            self.evaluate_event(&event);
+            self.refresh_screen()?;
+            if self.should_quit {
+                break;
             }
         }
-        disable_raw_mode().unwrap(); 
+        Ok(())
+    }
+    fn evaluate_event(&mut self, event: &Event) {
+        if let Key(KeyEvent {
+            code, modifiers, ..
+        }) = event
+        {
+            match code {
+                Char('q') if *modifiers == KeyModifiers::CONTROL => {
+                    self.should_quit = true;
+                }
+                _ => (),
+            }
+        }
+    }
+    fn refresh_screen(&self) -> Result<(), std::io::Error> {
+        if self.should_quit {
+            Self::clear_screen()?;
+            print!("Goodbye.\r\n");
+        }
+        Ok(())
     }
 }
+
+// https://flenker.blog/hecto-chapter-3/
